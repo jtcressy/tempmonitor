57450506932f637515a66d16f00ecb9e037ff34d
jtcressy <joel@jtcressy.net>
2018-05-05 20:38:42 -0600

    setup example react app

--- tempmonitor/web/static/js/App.jsx
+++ tempmonitor/web/static/js/App.jsx
@@ -1,8 +1,17 @@
 // App.jsx
 import React from "react";
+import ReactDOM from "react-dom";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import RaisedButton from "material-ui/RaisedButton";
 
 export default class App extends React.Component {
   render () {
-    return <p> Hello React!</p>;
+      return (
+      <MuiThemeProvider>
+          <p> Hello React!</p>
+          <RaisedButton label="default"/>
+      </MuiThemeProvider>
+  );
   }
 }
+
--- tempmonitor/web/static/js/AppIndex.jsx
+++ tempmonitor/web/static/js/AppIndex.jsx
@@ -1,4 +1,4 @@
-// index.jsx
+// AppIndex.jsx
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
